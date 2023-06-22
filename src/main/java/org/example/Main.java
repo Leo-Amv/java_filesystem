ae66f7ca0c3d1e128581bac37b828a26a3122f8d
Leo <leonidamvrosov@yandex.ru>
2023-06-22 21:11:51 +0500

    Tree added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Tree.print(new File("."), "", true);
+        Backup.backup("./backup");
     }
 }
